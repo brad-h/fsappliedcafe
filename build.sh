53dba8dfd531a3b970d9c6be0cfb976641d68c0a
brad <brad@juicebox>
2016-08-18 21:27:27 -0500

    add build script to pull down and run paket, ignore generated files

---
+++ build.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+url="https://github.com/fsprojects/Paket"
+if [ ! -d ".paket" ]; then
+  mkdir .paket
+  curl -L "$url/releases/download/3.13.3/paket.bootstrapper.exe" -o .paket/paket.bootstrapper.exe
+fi
+
+if test "$OS" = "Windows_NT"
+then # For Windows
+  .paket/paket.bootstrapper.exe
+  exit_code=$?
+  if [ $exit_code -ne 0 ]; then
+    exit $exit_code
+  fi
+else # For Non Windows
+  mono .paket/paket.bootstrapper.exe
+  exit_code=$?
+  if [ $exit_code -ne 0 ]; then
+    exit $exit_code
+  fi
+fi
