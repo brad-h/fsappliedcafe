2764e0a08557dcb160f79da0d3c135f2ac6e10c2
brad <brad@juicebox>
2016-08-23 19:02:51 -0500

    make sure both file and directory are there

--- build.sh
+++ build.sh
@@ -1,4 +1,12 @@
 #!/bin/bash
+url="https://github.com/fsprojects/Paket"
+if [ ! -d ".paket" ]; then
+  mkdir .paket
+fi
+
+if [ ! -e "paket.bootstrapper.exe" ]; then
+  curl -L "$url/releases/download/3.13.3/paket.bootstrapper.exe" -o .paket/paket.bootstrapper.exe
+fi
 
 if test "$OS" = "Windows_NT"
 then # For Windows
@@ -7,9 +15,19 @@ then # For Windows
   if [ $exit_code -ne 0 ]; then
     exit $exit_code
   fi
+  .paket/paket.exe restore
+  exit_code=$?
+  if [ $exit_code -ne 0 ]; then
+    exit $exit_code
+  fi
   packages/FAKE/tools/FAKE.exe $@ --fsiargs build.fsx
 else # For Non Windows
   mono .paket/paket.bootstrapper.exe
+  exit_code=$?
+  if [ $exit_code -ne 0 ]; then
+    exit $exit_code
+  fi
+  mono .paket/paket.exe restore
   exit_code=$?
   if [ $exit_code -ne 0 ]; then
     exit $exit_code
